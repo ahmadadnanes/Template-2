347b0e86e58108b765a0909b4b21681ed79fc850
ahmad adnan <aa264048@gmail.com>
2023-08-16 02:10:04 +0300

    add imageslider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let btn = document.querySelector("button");
 let ul = document.getElementById("classic");
-
 btn.onclick = function(){
     if(ul.className == "classic active"){
         ul.classList.remove("active");
@@ -28,3 +27,61 @@ up.onclick = () =>{
         behavior:"smooth"
     })
 }
+
+i = 0 ; images = [];
+
+images[0] = '../images/landing.jpg';
+images[1] = '../images/shuffle-04.jpg';
+images[2] = '../images/subscribe.jpg';
+
+bull1 = document.getElementById("bull1");
+bull2 = document.getElementById("bull2");
+bull3 = document.getElementById("bull3");
+land = document.getElementById("land");
+
+bull1.onclick = () =>{
+    land.style.backgroundImage = `url(${images[0]})`;
+    addactive(bull1);
+}
+
+bull2.onclick = () =>{
+    land.style.backgroundImage = `url(${images[1]})`;
+    addactive(bull2);
+}
+
+bull3.onclick = () =>{
+    land.style.backgroundImage = `url(${images[2]})`;
+    addactive(bull3);
+}
+
+function addactive(element){
+    if(element.className == "active" )
+        element.classList.remove("active");
+    else
+        element.classList.add("active");
+        
+}
+
+right = document.getElementById("right");
+left = document.getElementById("left");
+
+
+right.onclick = () =>{
+    land.style.backgroundImage = `url(${images[i]})`;
+
+    if(i < images.length - 1)
+        i++;
+    else
+        i = 0;
+}
+
+left.onclick = () =>{
+    land.style.backgroundImage = `url(${images[i]})`;
+
+    if(i > 0)
+        i--;
+    else
+        i = 0;
+}
+
+
