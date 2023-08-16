d0097c683f05cf75bd0c993d81031a9822d0223d
ahmad adnan <aa264048@gmail.com>
2023-08-16 17:26:59 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -35,55 +35,67 @@ images[0] = "../images/landing.jpg";
 images[1] = "../images/shuffle-04.jpg";
 images[2] = "../images/subscribe.jpg";
 
-bull1 = document.getElementById("bull1");
-bull2 = document.getElementById("bull2");
-bull3 = document.getElementById("bull3");
+bull = document.getElementsByClassName("bullet");
 land = document.getElementById("land");
-
-bull1.onclick = () =>{
-    land.style.backgroundImage = `url(${images[0]})`
-    addactive(bull1);
-}
-
-bull2.onclick = () =>{
-    land.style.backgroundImage = `url(${images[1]})`
-    addactive(bull2);
-}
-
-bull3.onclick = () =>{
-    land.style.backgroundImage = `url(${images[2]})`
-    addactive(bull3);
-}
-
-function addactive(element){
-    if(element.className == "active" )
-        element.classList.remove("active");
-    else
-        element.classList.add("active");
-        
-}
 
 right = document.getElementById("right");
 left = document.getElementById("left");
 
-
-right.onclick = () =>{
-    land.style.backgroundImage = `url(${images[i]})`
-
-    if(i < images.length - 1)
+right.addEventListener('click' , () =>{
+    land.style.backgroundImage = `url(${images[i]})`;
+    if(i < images.length - 1){
         i++;
-    else
+    }
+    else{
         i = 0;
-}
+    }
+    
+    if(i == 0){
+        bull[i].classList.add("active");
+        bull[i + 1].classList.remove("active");
+        bull[i + 2].classList.remove("active");
+    }
+    else if(i == 1){
+        bull[i].classList.add("active");
+        bull[i - 1].classList.remove("active");
+        bull[i + 1].classList.remove("active");
+    }
+    else{
+        bull[i].classList.add("active");
+        bull[i - 1].classList.remove("active");
+        bull[i - 2].classList.remove("active");
+    }
+})
 
-left.onclick = () =>{
-    land.style.backgroundImage = `url(${images[i]})`
-
-    if(i > 0)
+left.addEventListener('click' , () =>{
+    land.style.backgroundImage = `url(${images[i]})`;
+    if(i != 0){
         i--;
-    else
+    }
+    else{
         i = 0;
-}
+    }
+
+
+    if(i == 0){
+        bull[i].classList.add("active");
+        bull[i + 1].classList.remove("active");
+        bull[i + 2].classList.remove("active");
+    }
+    else if(i == 1){
+        bull[i].classList.add("active");
+        bull[i - 1].classList.remove("active");
+        bull[i + 1].classList.remove("active");
+    }
+    else{
+        bull[i].classList.add("active");
+        bull[i - 1].classList.remove("active");
+        bull[i - 2].classList.remove("active");
+    }
+})
+
+
+
 
 const media = window.matchMedia('(max-width: 1209px)');
 
